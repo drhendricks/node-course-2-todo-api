05094e963914e1a852326d4327dc6fe3ea35567e
Dylan Hendricks <d.r.hendricks2619@gmail.com>
2019-01-03 15:32:37 -0800

    Added server.js setup web server and post route

--- server/server.js
+++ server/server.js
@@ -1,32 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
-var newTodo = new Todo({
-    text: 'Jump up and down',
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
 });
 
-newTodo.save().then((doc) => {
-    console.log('Saved todo', doc);
-}, (e) => {
-    console.log('Unable to save todo');
-});
 
-var newUser = new User({
-    email: 'd.r.hendricks@hotmail.com'
-});
-var badUser = new User({
-    email: ''
-});
 
-newUser.save().then((doc) => {
-    console.log('Saved user', doc);
-}, (e) => {
-    console.log('Unable to save user', e);
-});
-
-badUser.save().then((doc) => {
-    console.log('Saved user', doc);
-}, (e) => {
-    console.log('Unable to save user', e);
+var port = 3000;
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
