fc794d8cf326ec46add46ea6a31c08d352a21ef3
Dylan Hendricks <d.r.hendricks2619@gmail.com>
2019-01-08 11:53:12 -0800

    Accidentally committed port change for local, switched back to use process.env.PORT for heroku deployment

--- server/server.js
+++ server/server.js
@@ -9,8 +9,8 @@ var {User} = require('./models/user');
 
 var app = express();
 
-//const port = process.env.PORT || 3000;
-const port = 3000;
+const port = process.env.PORT || 3000;
+//const port = 3000;
 
 app.use(bodyParser.json());
 
