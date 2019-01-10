70be6b77c0c6e40ed2955655c2b97691f87ad81b
Dylan Hendricks <d.r.hendricks2619@gmail.com>
2019-01-09 23:17:11 -0800

    Added new server configuration environments to enable using a new test database for unit testing

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,9 @@ var mongoose = require('mongoose');
 //Connect mongoose to database
 //tell mongoose to use built-in promise library
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
+
+
+process.env.NODE_ENV
