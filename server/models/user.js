03fcd355bf490f7fa508159cee5b9f5b11259139
Dylan Hendricks <d.r.hendricks2619@gmail.com>
2019-01-14 17:35:15 -0800

    Added new user token hashing scheme to user schema as well as changed POST /users route to use it

--- server/models/user.js
+++ server/models/user.js
@@ -1,14 +1,56 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-// User model
-// email - require it - trim it - string - min length of 1
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
     email: {
         require: true,
         trim: true,
         minlength: 1,
-        type: String
-    }
+        type: String,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        require: true,
+        type: String,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
+UserSchema.methods.toJSON = function () {
+    var user = this;
+    var userObject = user.toObject();
+    return _.pick(userObject, ['_id', 'email']);
+};
+
+//need a this keyword for instance functions
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+    user.tokens = user.tokens.concat([{access, token}]);
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+// User model
+// email - require it - trim it - string - min length of 1
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
